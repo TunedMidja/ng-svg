b0a6a7fd39924144d5fe368bcd9fa270b5edea65
TunedMidja <tunedmidja@gmail.com>
2017-10-06 16:17:11 +0200

    Added x-axis legend.

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -11,9 +11,10 @@ import * as _ from 'lodash';
     <div>
       <svg [attr.width]="width" [attr.height]="height">
         <svg:g>
-          <svg:line [attr.x1]="legendMargin" y1="0" [attr.x2]="legendMargin" [attr.y2]="width-legendMargin" [attr.stroke-width]="axisStrokeWidth" [attr.stroke]="axisColor"></svg:line>
-          <svg:line [attr.x1]="legendMargin" [attr.y1]="height-legendMargin" [attr.x2]="width" [attr.y2]="width-legendMargin" [attr.stroke-width]="axisStrokeWidth" [attr.stroke]="axisColor"></svg:line>
+          <svg:line [attr.x1]="legendMargin" y1="0" [attr.x2]="legendMargin" [attr.y2]="height-legendMargin" [attr.stroke-width]="axisStrokeWidth" [attr.stroke]="axisColor"></svg:line>
+          <svg:line [attr.x1]="legendMargin" [attr.y1]="height-legendMargin" [attr.x2]="width" [attr.y2]="height-legendMargin" [attr.stroke-width]="axisStrokeWidth" [attr.stroke]="axisColor"></svg:line>
           <svg:rect *ngFor="let stat of stats; index as i" [attr.x]="legendMargin + i * (barWidth + spaceBetweenBars)" [attr.y]="getYStart(stat)" [attr.height]="getHeight(stat)" [attr.width]="barWidth" />
+          <svg:text *ngFor="let stat of stats; index as i" [attr.x]="legendMargin + i * (barWidth + spaceBetweenBars)" [attr.y]="height-legendMargin+10" [attr.transform]="getXLegendRotation(stat, i)">{{stat.label}}</svg:text>
         </svg:g>
       </svg>
     </div>
@@ -26,10 +27,11 @@ import * as _ from 'lodash';
   `]
 })
 export class BarChartComponent implements OnInit, OnDestroy {
-  private width = 400;
+  private rightGraphMargin = 50;
+  private width = 400 + this.rightGraphMargin;
   private height = 400;
-  private legendMargin = 50;
-  private graphWidth = this.width - this.legendMargin;
+  private legendMargin = 100;
+  private graphWidth = this.width - this.legendMargin - this.rightGraphMargin;
   private graphHeight = this.height - this.legendMargin;
   private axisStrokeWidth = 2;
   private axisColor = 'black';
@@ -56,6 +58,14 @@ export class BarChartComponent implements OnInit, OnDestroy {
     this.statsSubscription.unsubscribe();
   }
 
+  private initBarChart(stats: Istat[]) {
+    this.stats = stats;
+    this.barWidth = this.graphWidth / (this.stats.length + this.stats.length / this.barToSpaceRatio);
+    this.spaceBetweenBars = this.barWidth / this.barToSpaceRatio;
+    const max = _.maxBy(this.stats, point => point.value);
+    this.verticalBarScale = this.graphHeight / max.value;
+  }
+
   private getHeight(point: Istat): number {
     return point.value * this.verticalBarScale;
   }
@@ -64,16 +74,7 @@ export class BarChartComponent implements OnInit, OnDestroy {
     return this.height - point.value * this.verticalBarScale - this.legendMargin;
   }
 
-  private initBarChart(stats: Istat[]) {
-    this.stats = stats;
-    this.barWidth = this.graphWidth / (this.stats.length + this.stats.length / this.barToSpaceRatio);
-    this.spaceBetweenBars = this.barWidth / this.barToSpaceRatio;
-
-    const max = _.maxBy(this.stats, point => {
-      console.log('point:', point);
-      return point.value;
-    });
-
-    this.verticalBarScale = this.graphHeight / max.value;
+  private getXLegendRotation(stat: Istat, i: number) {
+    return `rotate(30 ${this.legendMargin + i * (this.barWidth + this.spaceBetweenBars)} ${this.height - this.legendMargin/2})`;
   }
 }
